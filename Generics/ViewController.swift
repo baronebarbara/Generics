15028460d73837e3e1497eaee1bc730e115037f7
Barbara Barone <barbara.barone@picpay.com>
2021-07-15 16:05:20 -0300

    Initial Commit

---
+++ Generics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Generics
+//
+//  Created by Barbara Barone on 15/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
